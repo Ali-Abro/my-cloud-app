47005bdbfbfcb4fdf6fd60c343a3632ab762887c
Ali <rahimaliabro0@email.com>
2026-03-04 19:27:59 +0500

    add visit counter with database

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visits.db'
+db = SQLAlchemy(app)
+
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    new_visit = Visit()
+    db.session.add(new_visit)
+    db.session.commit()
+    count = Visit.query.count()
+    return render_template("index.html", name="Ali", count=count)
 
 @app.route("/about")
 def about():
-    return "This is Ali's first cloud deployment."
+    return render_template("about.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
